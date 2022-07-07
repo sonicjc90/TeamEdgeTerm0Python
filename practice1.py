950fb16cfae6b1545a040f1de2456bb87ad7ed4e
sonicjc90 <juan.medina@csedge.org>
2022-07-07 11:28:17 -0400

    lab 1 challenges

---
+++ practice1.py
@@ -0,0 +1,19 @@
+age = 17
+shiver = 'brrrrrrrrr'
+name = 'Sans'
+
+print('Hello World')
+print('I am an artificial AI')
+print('My name is Allegro')
+print('My internal dataframe was formated by my creator JC')
+print(age)
+
+#Will not work because of print only allows string and not int
+#print('I am' + age)
+
+print(f'I am {age}')
+print(f'My name is {name} and my age is {age}')
+
+#The command print will only recognize the first type of code - int or string etc.
+
+print(f'Big very cool gamer guy go {shiver} ')
--- variables.py
+++ variables.py
@@ -17,10 +17,13 @@ print(message)
 # -------------------------------------------- 
 
 print("\n------------------- Challenge 1 -------------------\n")
+name = 'JC' 
+grade = 10 
+funFact = 'I like to play video games' 
+print(f"Hi! My name is {name}", type(str))
+print(f"I'm in the {grade}th grade.", type(str))
+print(f"A fun fact about me is that {funFact}", type(str))
 
-#print(f"Hi! My name is {name}")
-#print(f"I'm in the {grade}th grade.")
-#print(f"A fun fact about me is that {funFact}")
 
 
 # **** Challenge 1: Problem 1 ****
@@ -34,11 +37,19 @@ print("\n------------------- Challenge 1 -------------------\n")
 
 
 
-# **** Challenge 1: Problem 2 ****
+print(' **** Challenge 1: Problem 2 ****')
 # Create a string for the different lines of the Happy Birthday Song
 # Print out the song using the strings you declared.
 
-
+you = 'Joseph'
+print('Happy Birthday to You')
+print('Happy Birthday to You')
+print(f'Happy Birthday Dear {you}')
+print('Happy Birthday to You.')
+print('From good friends and true,')
+print('From old friends and new,')
+print('May good luck go with you,')
+print('And happiness too.')
 
 
 # **** Challenge 1: Problem 3 ****
@@ -63,31 +74,49 @@ num2 = 10
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
 
+num3 = 10 + 5
+
+print(num3)
 
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
 
+num4 = 15 - 7
+
+print(num4)
 
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
 
+num5 = num4 * 3
+
+print(num5)
 
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
 
+num6 = pow(24, 2)
+
+print(num6)
 
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
 
+num7 = num6/4
+
+print(num7)
 
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
 
+num8 = num7%2
+
+print(num8)
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
